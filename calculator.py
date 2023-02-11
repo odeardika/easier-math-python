cd37d30c697a0343c9825c5d2682d868ad57393f
odeardika <dirfanardika@gmail.com>
2023-02-12 03:34:30 +0800

    Done with menu in calculator

--- calculator.py
+++ calculator.py
@@ -1,2 +1,46 @@
 def calculator():
     head = tail = 0
+    print("===================================================================")
+    print("|   + -> pertambah  |   * -> perkalian    |   ^ -> perpangkatan   |")
+    print("|   - -> pengurang  |   / -> perbagian    |   v -> pengakaran     |")
+    print("|   s -> sinus(sin) |   c -> kosinus(cos) |   t -> tangen(tan)    |")
+    print("|   S -> cosecan    |   C -> secan        |   T -> cotangen       |")
+    print("|   % -> persen     |   l -> logaritma    |   = -> hasil          |")
+    print("===================================================================")
+
+    while True:
+        choice = input("Pick the math operator : ")
+        if choice == "+":
+            pass
+        elif choice == "-":
+            pass
+        elif choice == "*":
+            pass
+        elif choice == "/":
+            pass
+        elif choice == "^":
+            pass
+        elif choice == "V":
+            pass
+        elif choice == "s":
+            pass
+        elif choice == "S":
+            pass
+        elif choice == "c":
+            pass
+        elif choice == "C":
+            pass
+        elif choice == "t":
+            pass
+        elif choice == "T":
+            pass
+        elif choice == "%":
+            pass
+        elif choice == "L":
+            pass
+        elif choice == "=":
+            pass
+        else:
+            print("Your choice is not in the menu!")
+
+
